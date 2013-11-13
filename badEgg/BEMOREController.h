a371874163e7df543f2ec00b52cfe69b69622970
lilin87788 <lilin87788@163.com>
2013-11-13 14:18:33 +0800

    MORE

---
+++ badEgg/BEMOREController.h
@@ -0,0 +1,13 @@
+//
+//  BEMOREController.h
+//  badEgg
+//
+//  Created by lilin on 13-11-13.
+//  Copyright (c) 2013年 surekam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEMOREController : UIViewController
+
+@end
---
+++ badEgg/BEMOREController.m
@@ -0,0 +1,42 @@
+//
+//  BEMOREController.m
+//  badEgg
+//
+//  Created by lilin on 13-11-13.
+//  Copyright (c) 2013年 surekam. All rights reserved.
+//
+
+#import "BEMOREController.h"
+
+@interface BEMOREController ()
+
+@end
+
+@implementation BEMOREController
+
+-(void)initNavBar
+{
+    self.navigationItem.backBarButtonItem.title = @"返回";
+    UIImage* navbgImage;
+    if (System_Version_Small_Than_(7)) {
+        navbgImage = [UIImage imageNamed:@"navbar441"];
+        
+    }else{
+        navbgImage = [UIImage imageNamed:@"navibar641"] ;
+    }
+    [self.navigationController.navigationBar setBackgroundImage:navbgImage  forBarMetrics:UIBarMetricsDefault];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self initNavBar];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
